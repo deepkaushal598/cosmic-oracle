229937c6328acfb54fc455a4af2112f96334bae4
Vansh <deepkaushal598@gmail.com>
2026-05-14 04:17:04 +0530

    fixed cors

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,9 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 const cosmicRoute = require("./routes/cosmicRoute");
